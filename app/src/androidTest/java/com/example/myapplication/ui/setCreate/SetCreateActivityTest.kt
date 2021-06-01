6b2800d539c00306716a76e04496570590a11560
Galeeva, Alina <galeeva.alm@gmail.com>
2021-06-01 23:45:50 +0300

    удалила комменты

--- app/src/androidTest/java/com/example/myapplication/ui/setCreate/SetCreateActivityTest.kt
+++ app/src/androidTest/java/com/example/myapplication/ui/setCreate/SetCreateActivityTest.kt
@@ -2,16 +2,16 @@ package com.example.myapplication.ui.setCreate
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.testing.launchFragment
+import android.view.View
 import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.core.app.launchActivity
-import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.matcher.RootMatchers.isDialog
 import androidx.test.espresso.matcher.ViewMatchers
@@ -19,14 +19,14 @@ import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.example.myapplication.R
+import com.example.myapplication.RegexMatcher
 import com.example.myapplication.ui.list.ListPageFragment
 import com.example.myapplication.ui.setView.SetViewActivity
+import org.hamcrest.Matcher
 import org.junit.After
-import org.junit.Test
-
-import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 
 class SetCreateActivityTest {
     @get:Rule
@@ -46,23 +46,15 @@ class SetCreateActivityTest {
         Intents.release()
     }
 
-    @Test
-    fun updateRecyclerViewInserted() {
-    }
 
     @Test
     fun showWordInputError() {
-        val bundle = Bundle()
-        val scenario = launchFragmentInContainer<ListPageFragment>()
-//        val scenario = launchFragment<ListPageFragment>()
-        onView(ViewMatchers.withId(R.id.fab)).perform(ViewActions.click())
-        onView(ViewMatchers.withId(R.id.edit_set_title)).perform(ViewActions.replaceText("Рассвет"))
-
-        onView(withText("OK"))
-        .inRoot(isDialog()) // <---
-            .check(matches(isDisplayed()))
-            .perform(click())
-
+        val intent = Intent(
+            ApplicationProvider.getApplicationContext(),
+            SetCreateActivity::class.java
+        ) .putExtra("settId", 2L)
+        var scenario = activityScenarioRule.scenario
+        scenario = launchActivity(intent)
 
         onView(ViewMatchers.withId(R.id.word_add_button)).perform(ViewActions.click())
         onView(ViewMatchers.withId(com.google.android.material.R.id.snackbar_text))
@@ -71,17 +63,56 @@ class SetCreateActivityTest {
 
     @Test
     fun showUndoDeleteWord() {
+        val intent = Intent(
+            ApplicationProvider.getApplicationContext(),
+            SetCreateActivity::class.java
+        )
+            .putExtra("settId", 2L)
+        var scenario = activityScenarioRule.scenario
+        scenario = launchActivity(intent)
+        onView(ViewMatchers.withId(R.id.original_input)).perform(ViewActions.replaceText("Рассвет"))
+        onView(ViewMatchers.withId(R.id.translated_input)).perform(
+            ViewActions.replaceText("Dawn"),
+            ViewActions.closeSoftKeyboard()
+        )
+        onView(ViewMatchers.withId(R.id.word_add_button)).perform(click())
+        onView(ViewMatchers.withId(R.id.recyclerivew_set_create)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                0,
+                ViewActions.swipeLeft()
+            )
+        )
+        onView(ViewMatchers.withId(com.google.android.material.R.id.snackbar_text))
+            .check(matches(withText("Рассвет удален")))
+
     }
 
-    @Test
-    fun hideKeyboard() {
-    }
 
-    @Test
-    fun cleanInputFields() {
-    }
 
     @Test
     fun showSuccessSavedToast() {
+
+        val intent = Intent(
+            ApplicationProvider.getApplicationContext(),
+            SetCreateActivity::class.java
+        )
+            .putExtra("settId", 2L)
+        var scenario = activityScenarioRule.scenario
+        scenario = launchActivity(intent)
+        onView(ViewMatchers.withId(R.id.original_input)).perform(ViewActions.replaceText("Рассвет"))
+        onView(ViewMatchers.withId(R.id.translated_input)).perform(
+            ViewActions.replaceText("Dawn"),
+            ViewActions.closeSoftKeyboard()
+        )
+        onView(ViewMatchers.withId(R.id.word_add_button)).perform(click())
+        onView(ViewMatchers.withId(R.id.recyclerivew_set_create)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                0,
+                ViewActions.swipeLeft()
+            )
+        )
+        onView(ViewMatchers.withId(com.google.android.material.R.id.snackbar_text))
+            .check(matches(withText("Рассвет удален")))
+
     }
 }
